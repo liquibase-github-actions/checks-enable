47d6125025a820be92204cd116bff2660eaeba81
liquibot <liquibot@liquibase.org>
2022-12-08 16:54:34 +0000

    auto generated v4.18.0

--- checks_enable.sh
+++ checks_enable.sh
@@ -255,4 +255,4 @@ if [[ -n "$USEPROCEDURESCHEMA" ]]; then
 	GLOBALS+=("--use-procedure-schema=$USEPROCEDURESCHEMA")
 fi
 
-docker-entrypoint.sh "${GLOBALS[@]}" checks enable "${PARAMS[@]}"
+docker-entrypoint.sh "${GLOBALS[@]}" pro checks enable "${PARAMS[@]}"
